01ca83861a37484fbf3d1ef852986b016cb182e5
לינוי <linoyhuta@gmail.com>
2026-08-20 11:58:45 +0300

    Screen

--- Screen.py
+++ Screen.py
@@ -1,12 +1,14 @@
 import tkinter as tk
+window = tk.Tk()
+
 def screen():
     background_img ="background.png"
-    window = tk.Tk()
     window.geometry("1920x1080")
     bg_image = tk.PhotoImage(background_img)
     canvas = tk.Canvas(window,width=1920 , height=1080)
     canvas.pack()
     canvas.create_image(0, 0, image=bg_image, anchor="nw")
+
 
 def click(event):
     if 820 <= event.x <= 1100 and 400 <= event.y <=680 :
